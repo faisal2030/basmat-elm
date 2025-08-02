f2c636d1ad5256a82336495d4868ffb81cac03ae
Faisal Alamlki <faisal.almalki1406@gmail.com_GitHub>
2025-08-02 11:02:13 +0300

    ربط قاعدة البيانات PostgreSQL مع Render

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
-        ssl_require=True if os.environ.get('RENDER') else False
+        ssl_require=False
     )
 }
 
